f4e86f37ccc895a23d006fe0ef0c5fbf52910762
lthreen <timothyjohan11@gmail.com>
2024-05-17 17:34:37 +0700

    Template and routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Home from './pages/Home'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Template from './components/Template'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Template />
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
