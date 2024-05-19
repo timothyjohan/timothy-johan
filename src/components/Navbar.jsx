df87c145a08c8e9009fbf408b7e2bafc6760b41b
lthreen <104489847+timothyjohan@users.noreply.github.com>
2024-05-19 17:12:09 +0700

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar(){
+    return(
+        <>
+            <div className="h-20 xl:mx-20 grid grid-cols-5 xl:grid-cols-2">
+                <div className="my-auto">
+                    <img src="/ado.jpg" className="xl:w-14 w-0 hide rounded-full" />
+                </div>
+                <div className="my-auto grid grid-cols-3 text-[#EEEEEE] font-bold col-span-4 xl:col-span-1">
+                    <div>About Me</div>
+                    <div>Experience</div>
+                    <div>My Projects</div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function Template(){
     return(
         <>
+            <Navbar />
             <Outlet></Outlet>
         </>
     )
