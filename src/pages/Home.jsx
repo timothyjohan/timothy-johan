cbfe83a35e351599873c24f09e416610ac36d761
lthreen <104489847+timothyjohan@users.noreply.github.com>
2024-05-19 16:52:26 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,25 +17,31 @@ export default function Home(){
                     </Fade>
                 </div>
             </div>
-
+            
             <div className="mx-10 xl:mx-44">
                 <div className="text-justify text-3xl text-[#EEEEEE] grid xl:grid-cols-2 md:grid-cols-2">
                     <div className="mb-20 xl:order-1">
-                        <h1 className="font-bold mb-8">About <span className="text-[#76ABAE]">Me</span></h1>
-                        <p className="text-xl mb-8">
-                            I am passionate about frontend web development and <span className="font-bold">UI/UX design</span>. I strive to create intuitive and engaging user interfaces by <span className="italic underline">putting myself in the user's shoes</span>  to understand their needs and desires. My goal is to design experiences that are not only <span className="font-bold"> visually appealing</span> but also <span className="font-bold">highly functional and user-friendly</span>.
-                        </p>
-                        <p className="text-xl mb-8">
-                            In addition to my focus on frontend development, I am also a dedicated <span className="font-bold">Computer Science</span> student with a keen interest in <span className="font-bold">machine learning</span>. I am continually expanding my knowledge and skills in this field, exploring how machine learning can be <span className="font-bold">integrated into web applications</span> to enhance user experiences and provide smarter solutions.
-                        </p>
-                        <p className="text-xl">
-                            I am excited about the endless possibilities in the intersection of web development and artificial intelligence, and I am committed to staying at the forefront of these evolving technologies.
-                        </p>
+                        <Fade cascade>
+
+                            <h1 className="font-bold mb-8">About <span className="text-[#76ABAE]">Me</span></h1>
+                            <p className="text-xl mb-8">
+                                I am passionate about frontend web development and <span className="font-bold">UI/UX design</span>. I strive to create intuitive and engaging user interfaces by <span className="italic underline">putting myself in the user's shoes</span>  to understand their needs and desires. My goal is to design experiences that are not only <span className="font-bold"> visually appealing</span> but also <span className="font-bold">highly functional and user-friendly</span>.
+                            </p>
+                            <p className="text-xl mb-8">
+                                In addition to my focus on frontend development, I am also a dedicated <span className="font-bold">Computer Science</span> student with a keen interest in <span className="font-bold">machine learning</span>. I am continually expanding my knowledge and skills in this field, exploring how machine learning can be <span className="font-bold">integrated into web applications</span> to enhance user experiences and provide smarter solutions.
+                            </p>
+                            <p className="text-xl">
+                                I am excited about the endless possibilities in the intersection of web development and artificial intelligence, and I am committed to staying at the forefront of these evolving technologies.
+                            </p>
+                        </Fade>
                     </div>
                     <div className="xl:order-2 hide md:mt-20 lg:ml-20">
-                        <img src="/blue-gear.webp" className="xl:w-80 w-60 ml-auto animate-spin" style={{animationDuration: '7s'}} />
-                        <img src="/blue-gear.webp" className="xl:w-64 w-44 ml-auto mr-20 animate-spin" style={{animationDuration: '5s'}} />
-                        <img src="/blue-gear.webp" className="xl:w-0 md:w-60 w-0 ml-auto animate-spin" style={{animationDuration: '6s'}} />
+                        <Fade>
+                            <img src="/blue-gear.webp" className="xl:w-80 w-60 ml-auto animate-spin" style={{animationDuration: '7s'}} />
+                            <img src="/blue-gear.webp" className="xl:w-64 w-44 ml-auto mr-20 animate-spin" style={{animationDuration: '5s'}} />
+                            <img src="/blue-gear.webp" className="xl:w-0 md:w-60 w-0 ml-auto animate-spin" style={{animationDuration: '6s'}} />
+
+                        </Fade>
                     </div>
                     
                 </div>
